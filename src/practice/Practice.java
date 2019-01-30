abe6cbce5aed62cd7640b3a5ba9fded4235c6d8c
Thomas Chapman <email.thomaschapman@gmail.com>
2019-01-30 11:22:10 -0500

    Fixed invalid creation of cards in the Card constructor

---
+++ src/practice/Practice.java
@@ -0,0 +1,16 @@
+package practice;
+
+/**
+ *
+ * @author thomas
+ */
+public class Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
