7936f970ef8224f1f71bf20b4861405832c8a92c
Thomas Chapman <email.thomaschapman@gmail.com>
2019-01-30 11:54:01 -0500

    Commit on my branch

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -7,6 +7,8 @@ package practice;
 public class Practice {
 
     /**
+     * Commit # 2 representing creation of my branch.
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
